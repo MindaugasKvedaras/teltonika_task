3a5d44a035346980174bcc1310afc612e96390f6
Minkcode <kvedaras.mindaugas@gmail.com>
2022-07-19 23:05:28 +0300

    fixed hamburger menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,33 +11,51 @@ const Layout = () => {
     <header>
         <Header />
     </header>
-      <nav className='app_navbar'>
-        <Hamburger color="#0078AA" fontSize={27} toggled={isOpen} toggle={setOpen} />
-        {isOpen && (
-          <div className='app_navbar-links'>
+        <div className='app_navbar-links_desktop'>
+              <div className='app_dropdown-menu'>
+                  <button className='app_dropdown-menu_home-btn'>
+                      <Link to="/">Add new developers</Link>
+                  </button>
+              </div>
             <div className='app_dropdown-menu'>
-                <button className='app_dropdown-menu_home-btn'>
-                    <Link to="/">Add new developers</Link>
-                </button>
+              <button className='app_dropdown-menu_btn'>Front-end developers</button>
+              <div className='app_dropdown-menu_content'>
+                  <Link to="/junior-front-end-developers">Junior</Link>
+                  <Link to="/senior-front-end-developers">Senior</Link>
+              </div>  
             </div>
-          <div className='app_dropdown-menu'>
-            <button className='app_dropdown-menu_btn'>Front-end developers</button>
-            <div className='app_dropdown-menu_content'>
-                <Link to="/junior-front-end-developers">Junior</Link>
-                <Link to="/senior-front-end-developers">Senior</Link>
-            </div>  
+            <div className='app_dropdown-menu'>
+              <button className='app_dropdown-menu_btn'>Back-end developers</button>
+              <div className='app_dropdown-menu_content'>
+                  <Link to="/junior-back-end-developers">Junior</Link>
+                  <Link to="/senior-back-end-developers">Senior</Link>
+              </div>  
+            </div>
           </div>
-          <div className='app_dropdown-menu'>
-            <button className='app_dropdown-menu_btn'>Back-end developers</button>
-            <div className='app_dropdown-menu_content'>
-                <Link to="/junior-back-end-developers">Junior</Link>
-                <Link to="/senior-back-end-developers">Senior</Link>
-            </div>  
+          <Hamburger color="#0078AA" fontSize={27} toggled={isOpen} toggle={setOpen} />
+          {isOpen && (
+            <div className='app_navbar-links'>
+              <div className='app_dropdown-menu'>
+                  <button className='app_dropdown-menu_home-btn'>
+                      <Link to="/">Add new developers</Link>
+                  </button>
+              </div>
+            <div className='app_dropdown-menu'>
+              <button className='app_dropdown-menu_btn'>Front-end developers</button>
+              <div className='app_dropdown-menu_content'>
+                  <Link to="/junior-front-end-developers">Junior</Link>
+                  <Link to="/senior-front-end-developers">Senior</Link>
+              </div>  
+            </div>
+            <div className='app_dropdown-menu'>
+              <button className='app_dropdown-menu_btn'>Back-end developers</button>
+              <div className='app_dropdown-menu_content'>
+                  <Link to="/junior-back-end-developers">Junior</Link>
+                  <Link to="/senior-back-end-developers">Senior</Link>
+              </div>  
+            </div>
           </div>
-        </div>
-        )}
-      </nav>
-
+          )}
       <Outlet />
     </>
   )
