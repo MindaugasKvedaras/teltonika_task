dce0e81c58977b046acb6b0d86fa32e2a89299bb
Minkcode <kvedaras.mindaugas@gmail.com>
2022-07-20 15:49:04 +0300

    fixed menubar closing bug again

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='app_header'>Teltonika task</div>
+    <div className='app_header'>Teltonika tasks</div>
   )
 }
 
