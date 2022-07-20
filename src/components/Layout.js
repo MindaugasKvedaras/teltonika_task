47e72145c5427954390dc08b8e0febc480cd740d
Minkcode <kvedaras.mindaugas@gmail.com>
2022-07-20 15:47:03 +0300

    fixed menubar closing bug again

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,6 +6,9 @@ import Footer from './Footer';
 
 const Layout = () => {
   const [isOpen, setOpen] = useState(false);
+  const closeHamburger = () => {
+    setOpen(false);
+  }
 
   return (
     <>
@@ -43,26 +46,26 @@ const Layout = () => {
             <div className='app_navbar-links'>
               <div className='app_dropdown-menu'>
                   <button className='app_dropdown-menu_home-btn'>
-                      <Link to="/">Add new developers</Link>
+                      <Link to="/" onClick={closeHamburger}>Add new developers</Link>
                   </button>
               </div>
             <div className='app_dropdown-menu'>
               <button className='app_dropdown-menu_btn'>Front-end developers</button>
               <div className='app_dropdown-menu_content'>
-                  <Link to="/junior-front-end-developers" onClick={isOpen}>Junior</Link>
-                  <Link to="/senior-front-end-developers" onClick={isOpen}>Senior</Link>
+                  <Link to="/junior-front-end-developers" onClick={closeHamburger}>Junior</Link>
+                  <Link to="/senior-front-end-developers" onClick={closeHamburger}>Senior</Link>
               </div>  
             </div>
             <div className='app_dropdown-menu'>
               <button className='app_dropdown-menu_btn'>Back-end developers</button>
               <div className='app_dropdown-menu_content'>
-                  <Link to="/junior-back-end-developers" onClick={isOpen}>Junior</Link>
-                  <Link to="/senior-back-end-developers" onClick={isOpen}>Senior</Link>
+                  <Link to="/junior-back-end-developers" onClick={closeHamburger}>Junior</Link>
+                  <Link to="/senior-back-end-developers" onClick={closeHamburger}>Senior</Link>
               </div>  
             </div>
             <div className='app_dropdown-menu'>
                   <button className='app_dropdown-menu_home-btn'>
-                      <Link to="/search-developer" onClick={isOpen}>Search developer</Link>
+                      <Link to="/search-developer" onClick={closeHamburger}>Search developer</Link>
                   </button>
               </div>
           </div>
