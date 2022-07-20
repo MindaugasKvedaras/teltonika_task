09bfbb1feaae48cb43e6d85953b438f94bf3538f
Minkcode <kvedaras.mindaugas@gmail.com>
2022-07-20 18:57:07 +0300

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import TeltonikaLogo from '../logo/teltonika_logo2.png';
 
 const Header = () => {
   return (
-    <div className='app_header'>Teltonika tasks</div>
+    <div className='app_header'>
+      <img src={TeltonikaLogo} alt="logo"/>
+    </div>
   )
 }
 
